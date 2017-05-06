80436c782b42b415be78aea9a58733796d0efe1f
witersong10 <witersong10@gmail.com>
2017-05-06 17:05:24 +0700

    Commit File

--- numpy/num1.py
+++ numpy/num1.py
@@ -1,10 +1,6 @@
 import pandas as pd
 import numpy as np
 s = pd.Series([1,3,5,np.nan,6,8])
-dates=  pd.date_range('20130101', periods=6
-pd.to_datetime("20170401",format='%Y%m%d');
+dates=  pd.date_range('20130101', periods=6)
 
-
-# test missing
-
-
+print(dates)
